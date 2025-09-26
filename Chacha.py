3174fab0c2f8ef6af86b156e1f3d04c88e1fe6a6
alegouix <ao-net@legouix.net>
2025-09-26 10:26:38 +0200

    squelette de Chacha

---
+++ Chacha.py
@@ -0,0 +1,16 @@
+class Chacha:
+    def __init__(self):
+        pass
+
+    def next_step(self):
+        pass
+
+
+
+
+def main():
+    c = Chacha()
+
+    while True:
+        c.next_step()
+        #affichage()
